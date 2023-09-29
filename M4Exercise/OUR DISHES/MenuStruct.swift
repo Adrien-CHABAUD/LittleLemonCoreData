b7e60e28f4eabb11f3d0389fa785d1119571f5c1
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2023-09-29 10:35:59 -0400

    Add structure for decoding the object received from the server.

--- M4Exercise/OUR DISHES/MenuStruct.swift
+++ M4Exercise/OUR DISHES/MenuStruct.swift
@@ -1,12 +1,13 @@
 import Foundation
 
 struct JSONMenu: Codable {
-    // add code here
+    let menu: [MenuItem]
 }
 
 
 struct MenuItem: Codable, Identifiable {
     let id = UUID()
     
-    // add code here
+    let title: String
+    let price: String
 }
