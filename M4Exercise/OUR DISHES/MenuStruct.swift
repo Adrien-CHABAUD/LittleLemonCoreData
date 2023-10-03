3df3f2e138806b3b15ce23ff3ce1d13ef2edc1b8
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2023-10-03 12:33:24 -0400

    Adding codingKeys

--- M4Exercise/OUR DISHES/MenuStruct.swift
+++ M4Exercise/OUR DISHES/MenuStruct.swift
@@ -2,12 +2,20 @@ import Foundation
 
 struct JSONMenu: Codable {
     let menu: [MenuItem]
+    
+    enum CodingKeys: String, CodingKey {
+        case menu = "menu"
+    }
 }
 
 
-struct MenuItem: Codable, Identifiable {
+struct MenuItem: Codable, Hashable, Identifiable {
     let id = UUID()
-    
     let title: String
     let price: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case price = "price"
+    }
 }
